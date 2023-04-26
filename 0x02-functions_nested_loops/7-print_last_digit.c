666ebe1c3a082225c867ab3e82d3ba1ed5f0399a
Raniah06 <raniafadil06@gmail.com>
2023-04-26 11:12:43 -0700

    commit message

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,11 +7,17 @@
  */
 int print_last_digit(int n)
 {
-int l = n % 10;
+int l;
+l = n % 10;
 if (l < 0)
 {
-l = -l;
+_putchar(-l + 48);
+return (-l);
 }
-_putchar((l + '0' + l);
+else
+{
+_putchar(l + 48);
 return (l);
 }
+}
+
