458044ed327890df1a7bbdd91195dbf0852aab31
Raniah06 <raniafadil06@gmail.com>
2023-04-26 03:48:26 -0700

    commit message

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_10x :- in lowercase ten times.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabetl_10x(void)
+{
+int i,j;
+for (j =0; j < 10; j++)
+{
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
+}
+_putchar('\n');
+}
