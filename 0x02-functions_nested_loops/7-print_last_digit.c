fb9c52937e0f3cdb08ee2e0fa2da57bd90763c05
Raniah06 <raniafadil06@gmail.com>
2023-04-26 07:44:58 -0700

    commit message

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * int print_last_digit(int n) - prints the last digit of number n
+ * @n: Number to be tested
+ *
+ * Return: last digit of n
+ */
+int print_last_digit(int n)
+{
+int l = n % 10;
+return (l);
+}
