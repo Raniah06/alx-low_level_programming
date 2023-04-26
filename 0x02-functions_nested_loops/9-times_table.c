4af54764cc0e0f01e9f84ce874660932928436cc
Raniah06 <raniafadil06@gmail.com>
2023-04-26 12:15:30 -0700

    commit message

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,11 +7,13 @@
 void times_table(void)
 {
 int i, j;
-for ( i = 0; i <= 9; i++)
+for (i = 0; i <= 9; i++)
 {
-for ( j = 0; j <= 9; i++)
+for (j = 0; j <= 9; i++)
 {
 _putchar(i * j);
+_putchar(',');
+_putchar(' ');
 }
 _putchar('\n');
 }
