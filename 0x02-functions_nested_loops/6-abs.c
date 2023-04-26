07cfdfa55a05853c8b26df09b7f456807d8a4ede
Raniah06 <raniafadil06@gmail.com>
2023-04-26 07:27:07 -0700

    commit message

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * int _abs(int n) - prints the sign of the given number +/-
+ * @n: Number to be tested
+ *
+ * Return: - n  if negative and n if positive
+ */
+int _abs(int n)
+{
+if (n < 0)
+{
+return (- n);
+}
+else
+{
+return (n);
+}
+}
+
+
