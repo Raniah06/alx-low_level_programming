6af3513a69af20123ab66f69b889c3672526543e
Raniah06 <raniafadil06@gmail.com>
2023-04-26 03:27:27 -0700

    commit message

---
+++ 0x02-functions_nested_loops/alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet :- in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+for (int i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
