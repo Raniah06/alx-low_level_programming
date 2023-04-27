db724ccd56968b5c7b04b76e22a0f91bde78a1ca
Raniah06 <raniafadil06@gmail.com>
2023-04-27 10:29:08 -0700

    commit message

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers between 0 to 9.
+ * Return: no return but print.
+ */
+void print_numbers(void)
+{
+	int d;
+
+	for (d = 48; ch < 58; ch++)
+	{
+		_putchar(d);
+	}
+	_putchar('\n');
+}
