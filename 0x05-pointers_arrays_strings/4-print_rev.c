75788dd641b84644da2153fe080c20d3b1672024
Raniah06 <raniafadil06@gmail.com>
2023-05-02 15:09:15 -0700

    commit message

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - returns the string in reverse 
+ *
+ * @s: this is the string
+ *
+ * Return: no return
+ */
+void print_rev(char *s)
+{
+int i, j;
+for (i = 0; s[i] != '\0' ; i++)
+;
+for ( j = i; j >= 0; j--)
+{
+_putchar(s[j]);
+}
+} 
