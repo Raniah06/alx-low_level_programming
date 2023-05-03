935901c0ab12697910439a5f1e00d795ba9c06c2
Raniah06 <raniafadil06@gmail.com>
2023-05-03 05:54:26 -0700

    commit message

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_array - returns the half string
+ *
+ * @a: this is the string
+ * @n: array size
+ *
+ * Return: no return
+ */
+
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n ; i++)
+{
+if (i = n - 1)
+{
+_putchar(a[i]);
+_putchar('\n');
+break;
+}
+_putchar(a[i]) ;
+_putchar(', ');
+}
+}
