12ec7ca6d5b4176fedc69d4c4e73fdd57e583ec5
Raniah06 <raniafadil06@gmail.com>
2023-05-03 13:18:39 -0700

    commit message

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,14 +1,15 @@
 #include "main.h"
  /**
- * _atoi - returns numbers
+ * _atoi- returns numbers in string
+ *
  * @s: this is the string
  *
- * Return: no return
+ * Return: return result
  */
 int _atoi(char *s)
 {
 int res = 0, i;
 for (i = 0; s[i] != '\0'; ++i)
 res = res * 10 + s[i] - '0';
-return res;
+return (res);
 }
