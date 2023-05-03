8f98ab9dd6a67c6a02c641a5618d5042bb1da070
Raniah06 <raniafadil06@gmail.com>
2023-05-03 00:26:02 -0700

    commit message

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,8 +8,13 @@
 void puts2(char *str)
 {
 int i;
-for (i = 0; str[i] != '\0' || str[i] = '\' ; i = i + 2)
+for (i = 0; str[i] != '\0'; i = i + 2)
 {
+if (str[i] == '\0')
+{
+_putchar('\n');
+break;
+}
 _putchar(str[i]);
 }
 _putchar('\n');
