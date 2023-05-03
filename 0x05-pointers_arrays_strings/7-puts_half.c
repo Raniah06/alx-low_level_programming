1cec5384ae22ee701ceb768aece9734294823a70
Raniah06 <raniafadil06@gmail.com>
2023-05-03 05:38:38 -0700

    commit message

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,7 +12,7 @@ void puts_half(char *str)
 int i, j;
 for (i = 0; str[i] != '\0' ; i++)
 ;
-for (j = ((i - 1) / 2) - 1; j <= i - 1; j++)
+for (j = ((i - 1) / 2) + 1; j <= i - 1; j++)
 {
 _putchar(str[j]);
 }
