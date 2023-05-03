cb8d41b34013db862965d498b0dbfb52844ea5e0
Raniah06 <raniafadil06@gmail.com>
2023-05-03 00:09:26 -0700

    commit message

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * puts2 - takes a pointerand updates its value.
+ * @str: input.
+ * Return: no return.
+ */
+void puts2(char *str)
+{
+for (i = 0; str[i] != '\0' ; i = i + 2)
+{
+	_putchar(str[i]);
+} 
+}
