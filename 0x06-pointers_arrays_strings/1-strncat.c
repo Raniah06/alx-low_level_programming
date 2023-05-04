7a775ba4fd5ac62e262507a9b2cf5e2435953952
Raniah06 <raniafadil06@gmail.com>
2023-05-04 06:16:47 -0700

    commit message

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenate two strings
+ * @dest: char string
+ * @src: char string
+ * @n: numbers to concatenate
+ * Return: pointer to dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+int i, j;
+
+for (i = 0; dest[i] != '\0'; i++)
+	;
+for (j = 0; j < n; j++)
+{
+dest[i] = src[j];
+i++;
+}
+return (dest);
+}
