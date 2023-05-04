05322a248ac0276770ae9265d153d6789c3cb780
Raniah06 <raniafadil06@gmail.com>
2023-05-04 14:12:42 -0700

    commit message

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize all words of a string
+ * @str: string
+ * Return: `str`
+ */
+
+char *cap_string(char *str)
+{
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+if(str[0] > 96 && str[0] < 123)
+c[0] -= 32;
+else if (str[i] = ' ' && str[i + 1] > 96 && str[i + 1] < 123)
+{
+str[i + 1] -= 32;
+}
+}
+return (c);
+}
