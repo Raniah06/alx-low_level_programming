e39d18665a30b600a062bd55661190e1761d2342
Raniah06 <raniafadil06@gmail.com>
2023-05-08 13:58:40 -0700

    commit message

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - Find first character
+ * @c: character to be finded
+ * Return: Pointer to array
+ */
+
+char *_strchr(char *s, char c)
+{
+     while (s != '\0')
+     {
+          if (*s == c)
+          {
+               return (s);
+          }
+          s++;
+     }
+     return (NULL);
+}
