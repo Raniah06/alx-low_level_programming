fb3af7f7bf18e391b1e327e0a92bdbfa9bf6b0f7
Raniah06 <raniafadil06@gmail.com>
2023-05-11 02:03:07 -0700

    commit message

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Print length of string
+ * @s: string
+ */
+
+int _strlen_recursion(char *s);
+{
+int c = 0;
+if (*s != '\0')
+{
+c++;
+_puts_recursion(++s);
+}
+return (c);
+}
