92d7f1a92359b3b80eea6bebf36b8ae265a48858
Raniah06 <raniafadil06@gmail.com>
2023-05-11 11:33:33 -0700

    commit message

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * is_palindrome - check if a string is a palindrome
+ * @s: char array string
+ * Return: 1 if palindrome, 0 if not
+ */
+
+int is_palindrome(char *s)
+{
+}
