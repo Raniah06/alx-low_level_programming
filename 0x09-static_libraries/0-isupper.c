33898be75febe6a6222c66d04d45dffe3d9bf195
Raniah06 <raniafadil06@gmail.com>
2023-05-17 09:00:44 -0700

    commit message

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * _isupper - check if a string is upper
+ * @c: character 
+ * Return:0 in success
+ */
+
 int _isupper(int c) {
   /* Function body will be added later */
   return 0;
