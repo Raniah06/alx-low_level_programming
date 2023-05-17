ad764fae90275210560aba8980cbe21977652754
Raniah06 <raniafadil06@gmail.com>
2023-05-17 11:47:55 -0700

    commit message

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - print the name of the file followed by a new line
+ * @argc: arguments
+ * @argv: arguments vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	return (0);
+}
