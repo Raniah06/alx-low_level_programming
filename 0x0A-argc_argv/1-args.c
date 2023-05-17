602bad4cf3e30b0d65cf9a5dbd5936e263517ada
Raniah06 <raniafadil06@gmail.com>
2023-05-17 09:51:11 -0700

    commit message

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print the name of the file followed by a new line
+ * @argc: arguments
+ * @argv: arguments vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argc - 1);
+
+	return (0);
+}
