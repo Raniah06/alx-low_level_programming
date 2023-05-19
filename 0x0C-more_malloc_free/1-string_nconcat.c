17e66c73c29ee2b57f16d42e8a9b41be4619858c
Raniah06 <raniafadil06@gmail.com>
2023-05-19 02:16:57 -0700

    commit message

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,24 +1,53 @@
 #include "main.h"
-#include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 
-char *string_nconcat(char *s1, char *s2, unsigned int n) {
-    if (s1 == NULL) s1 = "";
-    if (s2 == NULL) s2 = "";
+/**
+ * string_nconcat - concatenate 2 strings, only n bytes of s2
+ * @s1: string 1
+ * @s2: string 2
+ * @n: bytes to include of s2
+ * Return: NULL if fail, else pointer to malloc memory
+ */
 
-    size_t len1 = strlen(s1);
-    size_t len2 = strlen(s2);
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *p;
+	int len1, i, j;
 
-    if (n >= len2) n = len2;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    char *result = malloc(len1 + n + 1);
-    if (result == NULL) return (NULL);
+	len1 = (unsigned int)_strlen(s1);
+	p = malloc((len1 + n + 1) * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+	for (i = 0, j = 0; i < (len1 + n); i++)
+	{
+		if (i < len1)
+			p[i] = s1[i];
+		else
+			p[i] = s2[j++];
+	}
+	p[i] = '\0';
 
-    memcpy(result, s1, len1);
-   
-    memcpy(result + len1, s2, n);
+	return (p);
+}
 
-    result[len1 + n] = '\0';
+/**
+ * _strlen - find length of string
+ * @s: string
+ * Return: length of string
+ */
 
-    return (result);
+int _strlen(char *s)
+{
+int i = 0;
+
+while(s[i] != '\0')
+{
+i++;
+}
+return (i);
 }
