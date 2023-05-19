2fda5a9150606cc26bbf88e7bcb1f920943eb0d3
Raniah06 <raniafadil06@gmail.com>
2023-05-19 01:58:19 -0700

    commit message

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * malloc_checked - If malloc fails, terminate process with status 98
+ * malloc_checked - If malloc fails,terminate process with status 98
  * @b: unsigned integer
  * Return: void pointer
  */
 
 void *malloc_checked(unsigned int b)
 {
-	void *p = malloc(b);
-	if (p == NULL)
-		exit(98);
+void *p = malloc(b);
+if (p == NULL)
+exit(98);
 
-	return (p);
+return (p);
 }
