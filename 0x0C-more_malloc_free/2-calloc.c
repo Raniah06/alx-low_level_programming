111e2e134e3d16e73c829429d5a2cb5bba091598
Raniah06 <raniafadil06@gmail.com>
2023-05-19 11:23:00 -0700

    commit message

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,14 +13,14 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 char *p;
 unsigned int i;
 
-if (nmemb == 0 || size == 0)
+if (nmemb <= 0 || size <= 0)
 return (NULL);
 p = malloc(nmemb * size);
 if (p == NULL)
 return (NULL);
 
 i = 0;
-while (i < nmemb)
+while (i < nmemb * size)
 {
 p[i] = 0;
 i++;
