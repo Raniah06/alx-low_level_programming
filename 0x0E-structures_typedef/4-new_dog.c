997bdccd5cd6057e4ce78b7a336e555c23f34227
Raniah06 <raniafadil06@gmail.com>
2023-05-22 22:23:35 -0700

    commit message

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,12 +1,14 @@
 #include "dog.h"
 #include <stdlib.h>
+
 /**
-  * new_dog - creates a new struct of type dog
+  * new_dog - a new struct of type dog
   * @name: struct parameter name
   * @age: struct parameter age
   * @owner: struct parameter owner
   * Return: returns pointer to buffer of datatype dog
   */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
 int i, j, k, n;
@@ -16,22 +18,22 @@ for (i = 0; name[i] != '\0'; i++)
 ;
 for (j = 0; owner[j] != '\0'; j++)
 ;
-
 dogy = malloc(sizeof(dog_t));
 if (dogy == NULL)
 return (NULL);
-
 dogy->name = malloc(i  * sizeof(dogy->name));
-if (dogy == NULL)
+dogy->owner = malloc(j * sizeof(dogy->owner));
+if (dogy->name == NULL || dogy->owner == NULL)
+{
+free(do->name);
+free(do->owner);
+free(do);
 return (NULL);
+}
 for (k = 0; k <= i; k++)
 dogy->name[k] = name[k];
-
 dogy->age = age;
 
-dogy->owner = malloc(j * sizeof(dogy->owner));
-if (dogy == NULL)
-return (NULL);
 for (n = 0; n <= j; n++)
 dogy->owner[n] = owner[n];
 
