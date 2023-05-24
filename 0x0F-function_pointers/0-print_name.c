89244af68f5e59acc4ce84283a9efc5f171fae16
Raniah06 <raniafadil06@gmail.com>
2023-05-24 05:38:36 -0700

    commit message

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,9 @@
 #include "function_pointers.h"
 
 /**
- * print_name - Print a function pointer
- * @name: string name
- * @f: function pointer takes a string argument
+ * print_name - Print a name from a function pointer
+ * @name: char string
+ * @f: function pointer that takes a string argument
  */
 
 void print_name(char *name, void (*f)(char *))
