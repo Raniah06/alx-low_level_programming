b83d112f3acd6b13a0bb03d11b1f1ce96642f4f8
Raniah06 <raniafadil06@gmail.com>
2023-05-26 07:45:23 -0700

    commit message

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,30 +11,29 @@
 
 int main(int argc, char *argv[])
 {
-	int a, b;
-	int (*o)(int, int);
- o = get_op_func(argv[2]);
+int a, b;
+int (*o)(int, int);
+o = get_op_func(argv[2]);
 
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	if (argv[2][1] != '\0' || o == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+if (argv[2][1] != '\0' || o == NULL)
+{
+printf("Error\n");
+exit(99);
+}
 
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
+a = atoi(argv[1]);
+b = atoi(argv[3]);
  
-  if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	printf("%d\n", o(a, b));
-
-	return (0);
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+printf("%d\n", o(a, b));
+return (0);
 }
