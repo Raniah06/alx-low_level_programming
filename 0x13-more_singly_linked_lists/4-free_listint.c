93c9e3fdf964fd0d1157a4beff9a3d18d4a4f95d
Raniah06 <raniafadil06@gmail.com>
2023-11-22 07:04:49 -0800

    m

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -6,10 +6,12 @@
  */
 void free_listint(listint_t *head)
 {
+	listint_t *hold;
 
-while (head != NULL)
-{
-head = head->next;
-free(head);
-}
+	while (head != NULL)
+	{
+		hold = head;
+		head = head->next;
+		free(hold);
+	}
 }
