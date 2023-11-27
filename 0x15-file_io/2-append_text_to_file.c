5ee7f475938a9777625a15d363f1650cfc0deb10
Raniah06 <raniafadil06@gmail.com>
2023-11-27 12:13:04 -0800

    m

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,7 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	for (i = 0; text_content[i] != '\0'; i++)
 		;
-	bytesWritten = write(fd, text_content, i);
+	bytesWritten = write(file, text_content, i);
 	if (bytesWritten == -1)
 		return (-1);
 
