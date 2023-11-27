cea1e5825bc2543599695a67cad56f70b0f6d319
Raniah06 <raniafadil06@gmail.com>
2023-11-27 11:07:10 -0800

    m

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,10 +10,10 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
     int file;
-    char buffer[letters];
+    char *buffer;
     ssize_t bytesRead;
     ssize_t bytesWritten;
-
+    
     if (filename == NULL) {
         return (0);
     }
@@ -23,18 +23,27 @@ ssize_t read_textfile(const char *filename, size_t letters)
         return (0);
     }
 
+    buffer = (char *)malloc(letters * sizeof(char));
+    if (buffer == NULL) {
+        close(file);
+        return (0);
+    }
+
     bytesRead = read(file, buffer, letters);
     if (bytesRead <= 0) {
+        free(buffer);
         close(file);
         return (0);
     }
 
     bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
     if (bytesWritten != bytesRead) {
+        free(buffer);
         close(file);
         return (0);
     }
 
+    free(buffer);
     close(file);
     return (bytesRead);
 }
